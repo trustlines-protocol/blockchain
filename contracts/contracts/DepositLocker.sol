00fa811c2bc5fd11e93b3a1715f8722264c3343a
Ralf Schmitt <ralf@systemexit.de>
2019-04-12 16:28:02 +0200

    refactor the DepositLocker
    remove the deposit function. Each participant can now be registered by the
    auction contract when his bid is received. The auction contracts will deposit
    the total collected value with one call for all of the participants.
    
    We only keep state of the fact if an address is able to withdraw or not.
    The deposited amount is the same for all validators, so there is no need keep
    the amount for each validator. Slashing or withdrawing sets this flag to false.
    
    see https://github.com/trustlines-protocol/blockchain/issues/5#issuecomment-482539651

--- contracts/contracts/DepositLocker.sol
+++ contracts/contracts/DepositLocker.sol
@@ -3,27 +3,63 @@ pragma solidity ^0.4.25;
 import "./lib/Ownable.sol";
 import "./DepositLockerInterface.sol";
 
+/*
+  The DepositLocker contract locks the deposits for all of the winning
+  participants of the auction.
+
+  When the auction is running, the auction contract registers participants that
+  have successfully bid with the registerParticipant function. The DepositLocker
+  contracts keeps track of the number of participants and also keeps track if a
+  participant address can withdraw the deposit.
+
+  All of the participants have to pay the same eth amount when the auction ends.
+  The auction contract will deposit the sum of all amounts with a call to
+  depositAllBids.
+
+*/
+
 
 contract DepositLocker is DepositLockerInterface, Ownable {
 
     bool initialised = false;
+    bool bidsDeposited = false;
     address validatorSlasherAddress;
     address auctionContractAddress;
     uint public releaseBlockNumber;
-    mapping (address => uint) public deposits;
 
-    event Deposit(address depositOwner, uint value);
+    mapping (address => bool) public canWithdraw;
+    uint numberOfParticipants = 0;
+    uint closingPrice;
+    event ParticipantRegistered(address participantAddress, uint numberOfParticipants);
+    event Deposit(uint totalValue, uint closingPrice, uint numberOfParticipants);
     event Withdraw(address withdrawer, uint value);
     event Slash(address validator, uint slashedValue);
 
     modifier isInitialised() {
-        require(initialised, "The contract was not initiated.");
+        require(initialised, "The contract was not initialised.");
+        _;
+    }
+
+    modifier isBidsDeposited() {
+        require(bidsDeposited, "bids not deposited yet");
+        _;
+    }
+
+    modifier bidsNotDeposited() {
+        require(!bidsDeposited, "bids already deposited");
+        _;
+    }
+
+    modifier onlyAuctionContract() {
+        require(msg.sender == auctionContractAddress, "Only the AuctionContract can call this function.");
         _;
     }
 
     function() external {}
 
-    function init(uint _releaseBlockNumber, address _validatorSlasherAddress, address _auctionContractAddress) external onlyOwner returns (bool _success) {
+    function init(uint _releaseBlockNumber, address _validatorSlasherAddress, address _auctionContractAddress)
+        external onlyOwner returns (bool _success)
+    {
         require(! initialised, "The contract is already initialised.");
         require(_releaseBlockNumber > block.number, "The release block number cannot be lower or equal to the current block number");
 
@@ -35,28 +71,39 @@ contract DepositLocker is DepositLockerInterface, Ownable {
         return true;
     }
 
-    function deposit(address _sender) public payable isInitialised returns (bool _success) {
-        uint previousDeposit = deposits[_sender];
-        deposits[_sender] += msg.value;
-        require(previousDeposit <= deposits[_sender], "The value to be deposited overflows when added to the previous deposit.");
-        emit Deposit(_sender, msg.value);
+    function registerParticipant(address _participant) public isInitialised bidsNotDeposited onlyAuctionContract returns (bool _success) {
+        require(canWithdraw[_participant] == false, "can only register Participant once");
+        canWithdraw[_participant] = true;
+        numberOfParticipants += 1;
+        emit ParticipantRegistered(_participant, numberOfParticipants);
         return true;
     }
 
-    function withdraw() public isInitialised returns (bool _success) {
+    function depositAllBids(uint _closingPrice) public payable isInitialised bidsNotDeposited onlyAuctionContract returns (bool _success) {
+        require(numberOfParticipants>0, "no participants");
+        require(_closingPrice>0, "_closingPrice must be positive");
+        require(msg.value == numberOfParticipants * _closingPrice, "AuctionContract must sent right amount of money");
+        closingPrice = _closingPrice;
+        bidsDeposited = true;
+        emit Deposit(msg.value, closingPrice, numberOfParticipants);
+        return true;
+    }
+
+    function withdraw() public isInitialised isBidsDeposited returns (bool _success) {
         require(block.number >= releaseBlockNumber, "The deposit cannot be withdrawn yet.");
-        uint valueToSend = deposits[msg.sender];
-        deposits[msg.sender] = 0;
-        msg.sender.transfer(valueToSend);
-        emit Withdraw(msg.sender, valueToSend);
+        require(canWithdraw[msg.sender], "cannot withdraw from sender");
+
+        canWithdraw[msg.sender] = false;
+        msg.sender.transfer(closingPrice);
+        emit Withdraw(msg.sender, closingPrice);
         return true;
     }
 
-    function slash(address _validator) public isInitialised returns (bool _success) {
+    function slash(address _validator) public isInitialised isBidsDeposited returns (bool _success) {
         require(msg.sender == validatorSlasherAddress, "Only the ValidatorSlasher contract can call this function.");
-        uint previousDeposit = deposits[_validator];
-        deposits[_validator] = 0;
-        emit Slash(_validator, previousDeposit);
+        require(canWithdraw[_validator], "cannot withdraw from validator");
+        canWithdraw[_validator] = false;
+        emit Slash(_validator, closingPrice);
         return true;
     }
 }
