1e1f1afac69a977ac515254ebb36d5219981abf6
cducrest <come.ducrest@gmail.com>
2019-04-26 11:48:49 +0200

    Add ordering of deployment of contracts in deploy function Add initialization of contracts

--- auction-deploy/auction_deploy/cli.py
+++ auction-deploy/auction_deploy/cli.py
@@ -1,10 +1,15 @@
 import click
 from eth_keyfile import extract_key_from_keyfile
 from web3 import Web3
+from deploy_tools.deploy import send_function_call_transaction, deploy_compiled_contract
+from collections import namedtuple
 
 
-def decrypt_private_key(keystore: click.Path(), password: str):
-    return extract_key_from_keyfile(str(keystore), password.encode("utf-8"))
+ContractOptions = namedtuple(
+    "ContractOptions",
+    "start_price auction_duration number_of_participants release_block_number",
+)
+Contracts = namedtuple("Contracts", "locker slasher auction")
 
 
 jsonrpc_option = click.option(
@@ -31,7 +36,7 @@ gas_price_option = click.option(
     default=None,
 )
 nonce_option = click.option(
-    "--nonce", help="Nonce of the transaction to be sent", type=int, default=None
+    "--nonce", help="Nonce of the first transaction to be sent", type=int, default=None
 )
 
 
@@ -40,17 +45,37 @@ def main(prog_name="auction-deploy"):
     pass
 
 
-@main.command(short_help="Deploy validator auction contract")
+@main.command(
+    short_help="Deploys validator auction, deposit locker, and slasher contract. "
+    "Initializes the contracts."
+)
 @click.option(
     "--start-price",
-    help="Start Price of the auction in Ether",
+    help="Start Price of the auction in wei",
     type=int,
     show_default=True,
     default=10000,
 )
 @click.option(
     "--duration",
+    "auction_duration",
     help="Duration of the auction in days",
+    type=int,
+    show_default=True,
+    default=14,
+)
+@click.option(
+    "--participants",
+    "number_of_participants",
+    help="Duration of the auction in days",
+    type=int,
+    show_default=True,
+    default=14,
+)
+@click.option(
+    "--release-block",
+    "release_block_number",
+    help="The release block number of the deposit locker",
     type=int,
     show_default=True,
     default=14,
@@ -60,11 +85,19 @@ def main(prog_name="auction-deploy"):
 @gas_price_option
 @nonce_option
 @jsonrpc_option
-def deploy(start_price, duration, keystore, jsonrpc, gas, gas_price, nonce):
+def deploy(
+    start_price,
+    auction_duration,
+    number_of_participants,
+    release_block_number,
+    keystore,
+    jsonrpc,
+    gas,
+    gas_price,
+    nonce,
+):
 
-    web3 = Web3(  # noqa: F841
-        Web3.HTTPProvider(jsonrpc, request_kwargs={"timeout": 180})
-    )
+    web3 = Web3(Web3.HTTPProvider(jsonrpc, request_kwargs={"timeout": 180}))
     private_key = None
 
     if keystore is not None:
@@ -73,7 +106,106 @@ def deploy(start_price, duration, keystore, jsonrpc, gas, gas_price, nonce):
             type=str,
             hide_input=True,
         )
-        # password = b'test_password'
         private_key = decrypt_private_key(keystore, password)
 
-    click.echo(private_key)
+    contract_options = ContractOptions(
+        start_price, auction_duration, number_of_participants, release_block_number
+    )
+
+    transaction_options = {"gas": gas, "gasPrice": gas_price, "nonce": nonce}
+
+    contracts = deploy_contracts(
+        web3, transaction_options, private_key, contract_options
+    )
+
+    initialize_contracts(
+        contracts.locker, contracts.slasher, web3, transaction_options, private_key
+    )
+
+    click.echo("Auction address: " + contracts.auction.address)
+    click.echo("Deposit locker address: " + contracts.locker.address)
+    click.echo("Validator slasher address: " + contracts.slasher.address)
+
+
+def decrypt_private_key(keystore: click.Path(), password: str):
+    return extract_key_from_keyfile(str(keystore), password.encode("utf-8"))
+
+
+def increase_transaction_options_nonce(transaction_options):
+    if transaction_options["nonce"] is not None:
+        transaction_options["nonce"] = transaction_options["nonce"] + 1
+
+
+def deploy_contracts(web3, transaction_options, private_key, options):
+    deposit_locker_contract = deploy_compiled_contract(
+        abi=deposit_locker_abi,
+        bytecode=deposit_locker_bin,
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+    increase_transaction_options_nonce(transaction_options)
+
+    validator_slasher_contract = deploy_compiled_contract(
+        abi=validator_slasher_abi,
+        bytecode=validator_slasher_bin,
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+    increase_transaction_options_nonce(transaction_options)
+
+    auction_constructor_args = (
+        options.start_price,
+        options.auction_duration,
+        options.number_of_participants,
+        validator_slasher_contract.address,
+    )
+
+    auction_contract = deploy_compiled_contract(
+        abi=auction_abi,
+        bytecode=auction_bin,
+        web3=web3,
+        constructor_args=auction_constructor_args,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+    increase_transaction_options_nonce(transaction_options)
+
+    contracts = Contracts(
+        deposit_locker_contract, validator_slasher_contract, auction_contract
+    )
+
+    return contracts
+
+
+def initialize_contracts(
+    deposit_locker_contract,
+    release_block_number,
+    validator_slasher_contract,
+    web3,
+    transaction_options,
+    private_key,
+):
+
+    deposit_init = deposit_locker_contract.functions.init(
+        release_block_number, validator_slasher_contract.address
+    )
+    send_function_call_transaction(
+        deposit_init,
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+    increase_transaction_options_nonce(transaction_options)
+
+    slasher_init = validator_slasher_contract.functions.init(
+        deposit_locker_contract.address
+    )
+    send_function_call_transaction(
+        slasher_init,
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+    increase_transaction_options_nonce(transaction_options)
