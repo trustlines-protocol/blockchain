6aa372af8b6606ad5094018cfeba8b4d2482bb6c
cducrest <come.ducrest@gmail.com>
2019-05-02 17:15:30 +0200

    Add function to start the auction

--- auction-deploy/tests/test_cli.py
+++ auction-deploy/tests/test_cli.py
@@ -9,6 +9,20 @@ def runner():
     return CliRunner()
 
 
+@pytest.fixture
+def deployed_auction_address(runner):
+
+    deploy_result = runner.invoke(
+        main, args="deploy --release-block 789123 --jsonrpc test"
+    )
+
+    for line in deploy_result.output.split("\n")[-42:]:
+        if line.startswith("Auction address:"):
+            auction_address = line[-42:]
+
+    return auction_address
+
+
 def test_cli_contract_parameters_set(runner):
 
     result = runner.invoke(
@@ -40,18 +54,34 @@ def test_cli_private_key(runner, keystore_file_path, key_password):
     assert result.exit_code == 0
 
 
-@pytest.fixture
-def deployed_auction_address(runner):
+def test_cli_start_auction(runner, deployed_auction_address):
 
-    deploy_result = runner.invoke(
-        main, args="deploy --release-block 789123 --jsonrpc test"
+    result = runner.invoke(
+        main,
+        args="start-auction --jsonrpc test --auction-address "
+        + deployed_auction_address,
     )
 
-    for line in deploy_result.output.split("\n")[-42:]:
-        if line.startswith("Auction address:"):
-            auction_address = line[-42:]
+    assert result.exit_code == 0
 
-    return auction_address
+
+def test_cli_start_auction_key_not_owner(
+    runner, deployed_auction_address, keystore_file_path, key_password
+):
+    """Test that when you attempt to start the auction with a private_key not corresponding to the ow
+    ner of the auction, the command fails
+    This shows that the command takes into account the key"""
+
+    result = runner.invoke(
+        main,
+        args="start-auction --jsonrpc test --auction-address "
+        + deployed_auction_address
+        + " --keystore "
+        + str(keystore_file_path),
+        input=key_password,
+    )
+
+    assert result.exit_code == 1
 
 
 def test_cli_auction_status(runner, deployed_auction_address):
