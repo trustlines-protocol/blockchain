cdaca791157bcbc9b1a4025d48a628134e82e09c
Sascha Goebel <git@saschagoebel.com>
2019-05-22 14:02:53 +0200

    WIP

--- validator-set-deploy/tests/test_cli.py
+++ validator-set-deploy/tests/test_cli.py
@@ -10,8 +10,10 @@ def runner():
     return CliRunner()
 
 
-def test_dummy(runner):
+def test_deploy(runner):
 
     result = runner.invoke(main, args="deploy --jsonrpc test")
+
+    print(result.output)
 
     assert result.exit_code == 0
