ad7ecc1060a290e75e06fdb9d4d2e3913b48af12
cducrest <come.ducrest@gmail.com>
2019-05-27 13:29:24 +0200

    Prevent racing condition on allowance

--- contracts/contracts/TrustlinesNetworkToken.sol
+++ contracts/contracts/TrustlinesNetworkToken.sol
@@ -95,6 +95,7 @@ contract TrustlinesNetworkToken {
     function _approve(address owner, address spender, uint256 value) internal {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
+        require(value == 0 || allowed[owner][spender] == 0, "ERC20: approve only to or from 0 value");
 
         _allowances[owner][spender] = value;
         emit Approval(owner, spender, value);
