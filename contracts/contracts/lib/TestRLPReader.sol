bcf6d00419f7cedfed51d29f6e74e1753c72b4f8
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-06-04 17:00:47 +0200

    Upgrade rlp reader to newer version
    Upgrade the rlp reader lib to a newer version to fix a potentially
    security error in the lib when reading from an empty byte string.
    To be more save, we also revert on empty bytes.

---
+++ contracts/contracts/lib/TestRLPReader.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.5.8;
+
+/**
+ * The sole purpose of this file is to be able to test the internal functions
+ * of the RLPReader library contract.
+ */
+
+import "./RLPReader.sol";
+
+
+contract TestRLPReader {
+
+    function() external {}
+
+    function testToRlpItem(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (uint length, uint memPtr)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return (rlpItem.len, rlpItem.memPtr);
+    }
+
+    function testIsList(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (bool)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.isList(rlpItem);
+    }
+
+    function testToBoolean(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (bool)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.toBoolean(rlpItem);
+    }
+
+    function testToBytes(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (bytes memory)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.toBytes(rlpItem);
+    }
+
+    function testToAddress(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (address)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.toAddress(rlpItem);
+    }
+
+    function testToUint(bytes memory _rlpEncodedItem)
+        public
+        pure
+        returns (uint)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.toUint(rlpItem);
+    }
+
+    function testGetItemUint(uint index, bytes memory _rlpEncodedItem)
+    public
+    pure
+    returns (uint)
+    {
+        RLPReader.RLPItem memory rlpItem = RLPReader.toRlpItem(_rlpEncodedItem);
+        return RLPReader.toUint(RLPReader.toList(rlpItem)[index]);
+    }
+}
