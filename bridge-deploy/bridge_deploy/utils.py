c3ac553c0ee224cc4f62acb6a34752e5fcebb398
Sascha Goebel <git@saschagoebel.com>
2019-06-25 10:27:50 +0200

    Move poa-bridge-contracts submodule into submodules folder

--- bridge-deploy/bridge_deploy/utils.py
+++ bridge-deploy/bridge_deploy/utils.py
@@ -6,7 +6,7 @@ script_directory = path.dirname(path.realpath(__file__))
 
 def load_poa_contract(contract_name):
     with open(
-        f"{script_directory}/../../poa-bridge-contracts/build/contracts/{contract_name}.json"
+        f"{script_directory}/../../submodules/poa-bridge-contracts/build/contracts/{contract_name}.json"
     ) as json_file:
         return json.load(json_file)
 
