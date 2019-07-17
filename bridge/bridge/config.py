1ad9b055ab18f8d292f3c6a045e377ce4c9f873e
Jannik Luhn <jannik@brainbot.com>
2019-07-17 14:57:23 +0200

    Improve config validation

--- bridge/bridge/config.py
+++ bridge/bridge/config.py
@@ -1,14 +1,31 @@
+from typing import Any, Dict
+
 import toml
 
 from eth_utils.toolz import merge
 
 
-REQUIRED_CONFIG_ENTRIES = ["home-rpc-url", "foreign-rpc-url"]
+def validate_rpc_url(url: Any) -> str:
+    if not isinstance(url, str):
+        raise ValueError(f"{url} is not a valid RPC url")
+
+
+REQUIRED_CONFIG_ENTRIES = ["home_rpc_url", "foreign_rpc_url"]
 
 OPTIONAL_CONFIG_ENTRIES_WITH_DEFAULTS = {}
 
+CONFIG_ENTRY_VALIDATORS = {
+    "home_rpc_url": validate_rpc_url,
+    "foreign_rpc_url": validate_rpc_url,
+}
 
-def load_config(path):
+assert all(key in CONFIG_ENTRY_VALIDATORS for key in REQUIRED_CONFIG_ENTRIES)
+assert all(
+    key in CONFIG_ENTRY_VALIDATORS for key in OPTIONAL_CONFIG_ENTRIES_WITH_DEFAULTS
+)
+
+
+def load_config(path: str) -> Dict[str, Any]:
     if path is None:
         user_config = {}
     else:
@@ -21,7 +38,7 @@ def load_config(path):
     return config
 
 
-def validate_config(config):
+def validate_config(config: Dict[str, Any]) -> None:
     # check for missing keys
     for required_key in REQUIRED_CONFIG_ENTRIES:
         if required_key not in config:
@@ -34,3 +51,10 @@ def validate_config(config):
             and key not in OPTIONAL_CONFIG_ENTRIES_WITH_DEFAULTS
         ):
             raise ValueError(f"Config contains unknown key {key}")
+
+    # check for validity of entries
+    for key, value in config.items():
+        try:
+            CONFIG_ENTRY_VALIDATORS[key](value)
+        except ValueError as value_error:
+            raise ValueError(f"Invalid config entry {key}: {value_error}")
--- bridge/bridge/main.py
+++ bridge/bridge/main.py
@@ -6,16 +6,19 @@ from bridge.config import load_config
 
 @click.command()
 @click.option(
-    "-c", "--config", "config_path", type=click.Path(exists=True), default=None
+    "-c",
+    "--config",
+    "config_path",
+    type=click.Path(exists=True),
+    default=None,
+    help="Path to a config file",
 )
-def main(config_path):
+def main(config_path: str) -> None:
     try:
         config = load_config(config_path)
     except toml.decoder.TomlDecodeError as decode_error:
-        raise click.UsageError(
-            f"Error in config file: {decode_error}"
-        ) from decode_error
+        raise click.UsageError(f"Invalid config file: {decode_error}") from decode_error
     except ValueError as value_error:
-        raise click.UsageError(f"Error in config file: {value_error}") from value_error
+        raise click.UsageError(f"Invalid config file: {value_error}") from value_error
 
     print(config)
