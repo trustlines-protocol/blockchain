7c14f0b0d0ae60e9314ce881602b7542eff93a34
Thore Weilbier <thore@weilbier.net>
2019-07-19 13:33:13 +0200

    Adjust bridge transfer fetcher to use eth_getLogs
    This includes some additional minor review suggestions.
    
    Related to: trustlines-network/project#582
    See also: #240

--- tools/bridge/bridge/main.py
+++ tools/bridge/bridge/main.py
@@ -46,7 +46,7 @@ def main(config_path: str) -> None:
         w3_foreign=w3_foreign,
         token_contract_address=config["token_contract_address"],
         foreign_bridge_contract_address=config["foreign_bridge_contract_address"],
-        max_reorg_depth=config["max_reorg_depth"],
+        foreign_chain_max_reorg_depth=config["foreign_chain_max_reorg_depth"],
         transfer_event_fetch_limit=config["transfer_event_fetch_limit"],
     )
 
