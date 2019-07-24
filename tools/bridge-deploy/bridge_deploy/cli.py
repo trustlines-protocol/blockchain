c56942afeb3cd2090252e975495d4082bdd9afe8
Thore Weilbier <thore@weilbier.net>
2019-07-24 16:26:18 +0200

    Add updated bridge deploy tool home target
    Related to: trustlines-network/project#595

--- tools/bridge-deploy/bridge_deploy/cli.py
+++ tools/bridge-deploy/bridge_deploy/cli.py
@@ -14,7 +14,10 @@ from deploy_tools.cli import (
 from deploy_tools.deploy import build_transaction_options
 from deploy_tools.files import validate_and_format_address, InvalidAddressException
 
-from bridge_deploy.core import deploy_foreign_bridge_contract
+from bridge_deploy.core import (
+    deploy_foreign_bridge_contract,
+    deploy_home_bridge_contract,
+)
 
 
 def validate_address(ctx, param, value):
@@ -33,6 +36,24 @@ token_address_option = click.option(
     type=str,
     required=True,
     callback=validate_address,
+)
+
+validator_proxy_address_option = click.option(
+    "--validator-proxy-address",
+    help=("The address of the ValidatorProxy contract ('0x' prefixed string)."),
+    type=str,
+    required=True,
+    callback=validate_address,
+)
+
+validators_required_percent_option = click.option(
+    "--validators-required-percent",
+    help=(
+        "The relative amount of validators required to confirm a bridge transfer"
+        "(positive integer in range 0 till 100)"
+    ),
+    type=int,
+    default=50,
 )
 
 
@@ -79,6 +100,48 @@ def deploy_foreign(
     )
 
     click.echo(f"ForeignBridge address: {foreign_bridge_contract.address}")
+    click.echo(f"  deployed at block #{web3.eth.blockNumber}")
+
+
+@main.command(short_help="Deploys the token bridge on the home network.")
+@keystore_option
+@gas_option
+@gas_price_option
+@nonce_option
+@auto_nonce_option
+@jsonrpc_option
+@validator_proxy_address_option
+@validators_required_percent_option
+def deploy_home(
+    keystore: str,
+    gas: int,
+    gas_price: int,
+    nonce: int,
+    auto_nonce: bool,
+    jsonrpc: str,
+    validator_proxy_address: str,
+    validators_required_percent: int,
+) -> None:
+
+    web3 = connect_to_json_rpc(jsonrpc)
+    private_key = retrieve_private_key(keystore)
+
+    nonce = get_nonce(
+        web3=web3, nonce=nonce, auto_nonce=auto_nonce, private_key=private_key
+    )
+    transaction_options = build_transaction_options(
+        gas=gas, gas_price=gas_price, nonce=nonce
+    )
+
+    home_bridge_contract = deploy_home_bridge_contract(
+        validator_proxy_contract_address=validator_proxy_address,
+        validators_required_percent=validators_required_percent,
+        web3=web3,
+        transaction_options=transaction_options,
+        private_key=private_key,
+    )
+
+    click.echo(f"HomeBridge address: {home_bridge_contract.address}")
     click.echo(f"  deployed at block #{web3.eth.blockNumber}")
 
 
--- tools/bridge-deploy/tests/test_cli.py
+++ tools/bridge-deploy/tests/test_cli.py
@@ -10,18 +10,40 @@ def runner():
     return CliRunner()
 
 
-def test_deploy_foreign(runner):
-
+def test_deploy_foreign(runner, abitrary_address):
     result = runner.invoke(
-        main,
-        args="deploy-foreign --jsonrpc test --token-address 0x5757957701948584cc2A8293857D89b19De44f0F",
+        main, args=f"deploy-foreign --jsonrpc test --token-address {abitrary_address}"
     )
 
     assert result.exit_code == 0
 
 
 def test_print_foreign(runner):
+    result = runner.invoke(main, args=f"print-foreign --jsonrpc test")
 
-    result = runner.invoke(main, args="print-foreign --jsonrpc test")
+    assert result.exit_code == 0
+
+
+def test_deploy_home(runner, abitrary_address):
+    result = runner.invoke(
+        main,
+        args=(
+            "deploy-home --jsonrpc test"
+            f" --validator-proxy-address {abitrary_address}"
+        ),
+    )
+
+    assert result.exit_code == 0
+
+
+def test_deploy_home_with_required_percentage_argument(runner, abitrary_address):
+    result = runner.invoke(
+        main,
+        args=(
+            "deploy-home --jsonrpc test"
+            f" --validator-proxy-address {abitrary_address}"
+            " --validators-required-percent 20"
+        ),
+    )
 
     assert result.exit_code == 0
