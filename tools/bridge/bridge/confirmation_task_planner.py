54c29883ec2ff5c9f19156053afde924c4a413bf
Jannik Luhn <jannik@brainbot.com>
2019-08-02 03:23:03 +0200

    Improve task planner

--- tools/bridge/bridge/confirmation_task_planner.py
+++ tools/bridge/bridge/confirmation_task_planner.py
@@ -1,13 +1,11 @@
-from typing import List, Set
+from typing import List, Set, NamedTuple
 
 from eth_typing import Hash32
 
-
-MAX_CLOCK_DISPARITY = 60  # maximum lock disparity between home and foreign chain
-HISTORY_TIME = 60 * 60  # forget about events that are older than this time
+from eth_utils import decode_hex
 
 
-class TimestampedTransferHash:
+class TimestampedTransferHash(NamedTuple):
     timestamp: int
     transfer_hash: Hash32
 
@@ -17,18 +15,23 @@ class TransferHashRecorder:
         self.timestamped_transfer_hashes: List[TimestampedTransferHash] = []
         self.current_timestamp = 0
 
-    def apply_event_batch(self, timestamp, events):
+    def record_event(self, timestamp, event):
+        """Record an event or record that no further event happened until the given timestamp.
+
+        Events or non-events must be recorded in order.
+        """
         if timestamp < self.current_timestamp:
-            raise ValueError(f"Tried to apply events out of order")
+            raise ValueError(f"Tried to apply event out of order")
         self.current_timestamp = timestamp
 
-        for event in events:
+        if event is not None:
             timestamped_transfer_hash = TimestampedTransferHash(
-                timestamp, events.args.transferHash
+                timestamp, decode_hex(event["args"]["transferHash"])
             )
             self.timestamped_transfer_hashes.append(timestamped_transfer_hash)
 
     def forget_transfer_hash(self, transfer_hash: Hash32) -> None:
+        """Drop a specific transfer hash."""
         self.timestamped_transfer_hashes = [
             timestamped_transfer_hash
             for timestamped_transfer_hash in self.timestamped_transfer_hashes
@@ -36,53 +39,74 @@ class TransferHashRecorder:
         ]
 
     def forget_old_transfer_hashes(self, cutoff_timestamp: int) -> None:
+        """Drop all transfer hashes that happened before the given cutoff timestamp."""
         self.timestamped_transfer_hashes = [
             timestamped_transfer_hash
             for timestamped_transfer_hash in self.timestamped_transfer_hashes
-            if timestamped_transfer_hash.timestamp < cutoff_timestamp
+            if timestamped_transfer_hash.timestamp >= cutoff_timestamp
         ]
 
     def transfer_hash_exists(self, transfer_hash: Hash32) -> bool:
+        """Check if a transfer hash has been recorded and not yet forgotten or not."""
         return any(
             timestamped_transfer_hash.transfer_hash == transfer_hash
             for timestamped_transfer_hash in self.timestamped_transfer_hashes
         )
 
+    def get_transfer_hashes(self, start_time: int, end_time: int) -> List[Hash32]:
+        """Return all transfer hashes in a given (inclusive) time interval."""
+        return [
+            transfer_hash
+            for timestamp, transfer_hash in self.timestamped_transfer_hashes
+            if start_time <= timestamp <= end_time
+        ]
+
 
 class ConfirmationTaskPlanner:
-    def __init__(self):
+    def __init__(self, home_look_ahead: int, history_length: int) -> None:
         self.transfer_event_recorder = TransferHashRecorder()
         self.confirmation_event_recorder = TransferHashRecorder()
         self.completion_event_recorder = TransferHashRecorder()
 
-        self.recently_handled_transfer_hashes: Set[TimestampedTransferHash] = set()
+        self.recently_processed_transfer_hashes: Set[Hash32] = set()
 
-    def get_next_transfers_to_confirm(self) -> List[Hash32]:
-        transfer_hashes_to_confirm = []
+        self.home_look_ahead = home_look_ahead
+        self.history_length = history_length
 
-        for (
-            timestamped_transfer_hash
-        ) in self.transfer_event_recorder.timestamped_transfer_hashes:
-            timestamp, transfer_hash = timestamped_transfer_hash
+        self.transfers_processed_until = 0
 
-            # stop if home chain syning isn't sufficiently far yet
-            if (
-                timestamp
-                > self.confirmation_event_recorder.current_timestamp
-                - MAX_CLOCK_DISPARITY
-            ):
-                break
-            if (
-                timestamp
-                > self.completion_event_recorder.current_timestamp - MAX_CLOCK_DISPARITY
-            ):
-                break
+    def apply_event(self, timestamp: int, event: dict) -> None:
+        if event["event"] == "Transfer":
+            recorder = self.transfer_event_recorder
+        elif event["event"] == "Confirmation":
+            recorder = self.confirmation_event_recorder
+        elif event["event"] == "Completion":
+            recorder = self.completion_event_recorder
+        else:
+            raise ValueError("Got unknown event {event.event}")
 
-            # ignore this transfer if we've handled it already
-            if transfer_hash in self.recently_handled_transfer_hashes:
-                continue
+        recorder.record_event(timestamp, event)
 
-            # check if we need to confirm it
+    def get_next_transfer_hashes(self) -> List[Hash32]:
+        transfer_time = self.transfer_event_recorder.current_timestamp
+        confirmation_time = self.confirmation_event_recorder.current_timestamp
+        completion_time = self.completion_event_recorder.current_timestamp
+
+        home_time = min(confirmation_time, completion_time)
+
+        start_time = self.transfers_processed_until
+        end_time = home_time - self.home_look_ahead
+
+        transfer_hashes = self.transfer_event_recorder.get_transfer_hashes(
+            start_time=start_time, end_time=end_time
+        )
+        self.transfers_processed_until = transfer_time
+
+        new_transfer_hashes = []
+        for transfer_hash in transfer_hashes:
+            recently_processed = (
+                transfer_hash in self.recently_processed_transfer_hashes
+            )
             already_confirmed = self.confirmation_event_recorder.transfer_hash_exists(
                 transfer_hash
             )
@@ -90,11 +114,15 @@ class ConfirmationTaskPlanner:
                 transfer_hash
             )
 
-            if already_confirmed or already_completed:
-                self.recently_handled_transfer_hashes.add(timestamped_transfer_hash)
-                transfer_hashes_to_confirm.append(transfer_hash)
+            if (
+                not recently_processed
+                and not already_confirmed
+                and not already_completed
+            ):
+                new_transfer_hashes.append(transfer_hash)
+                self.recently_processed_transfer_hashes.add(transfer_hash)
 
-        return transfer_hashes_to_confirm
+        return new_transfer_hashes
 
     def clear_history(self):
         cutoff_time = (
@@ -103,33 +131,26 @@ class ConfirmationTaskPlanner:
                 self.confirmation_event_recorder.current_timestamp,
                 self.completion_event_recorder.current_timestamp,
             )
-            - HISTORY_TIME
+            - self.history_length
         )
-        transfer_hashes_to_clear = set(
-            transfer_hash
-            for transfer_hash, timestamp in self.recently_handled_transfer_hashes
-            if timestamp < cutoff_time
-        )
-        self.recently_handled_transfer_hashes -= transfer_hashes_to_clear
 
         self.transfer_event_recorder.forget_old_transfer_hashes(cutoff_time)
         self.confirmation_event_recorder.forget_old_transfer_hashes(cutoff_time)
         self.completion_event_recorder.forget_old_transfer_hashes(cutoff_time)
 
-    def apply_transfer_event_batch(self, timestamp, events):
-        self.transfer_event_recorder.apply_event_batch(timestamp, events)
-        transfer_hashes_to_confirm = self.get_next_transfers_to_confirm()
-        self.clear_history()
-        return transfer_hashes_to_confirm
-
-    def apply_confirmation_event_batch(self, timestamp, events):
-        self.confirmation_event_recorder.apply_event_batch(timestamp, events)
-        transfer_hashes_to_confirm = self.get_next_transfers_to_confirm()
-        self.clear_history()
-        return transfer_hashes_to_confirm
-
-    def apply_completion_event_batch(self, timestamp, events):
-        self.completion_event_recorder.apply_event_batch(timestamp, events)
-        transfer_hashes_to_confirm = self.get_next_transfers_to_confirm()
-        self.clear_history()
-        return transfer_hashes_to_confirm
+        cleared_transfer_hashes = set(
+            transfer_hash
+            for transfer_hash in self.recently_processed_transfer_hashes
+            if not self.transfer_event_recorder.transfer_hash_exists(transfer_hash)
+        )
+        processed_transfer_hashes = set(
+            transfer_hash
+            for transfer_hash in self.recently_processed_transfer_hashes
+            if all(
+                self.transfer_event_recorder.transfer_hash_exists(transfer_hash),
+                self.confirmation_event_recorder.transfer_hash_exists(transfer_hash),
+                self.completion_event_recorder.transfer_hash_exists(transfer_hash),
+            )
+        )
+        self.recently_processed_transfer_hashes -= cleared_transfer_hashes
+        self.recently_processed_transfer_hashes -= processed_transfer_hashes
