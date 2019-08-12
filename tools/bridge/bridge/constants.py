a4e2e5ac0c2ec8e5364f82ab911f099e0d35c4ea
Jannik Luhn <jannik@brainbot.com>
2019-08-12 21:02:29 +0200

    Add comment for confirmation tx gas limit constant

--- tools/bridge/bridge/constants.py
+++ tools/bridge/bridge/constants.py
@@ -4,4 +4,6 @@ TRANSFER_EVENT_NAME = "Transfer"
 CONFIRMATION_EVENT_NAME = "Confirmation"
 COMPLETION_EVENT_NAME = "TransferCompleted"
 
-CONFIRMATION_TRANSACTION_GAS_LIMIT = 300_000
+CONFIRMATION_TRANSACTION_GAS_LIMIT = (
+    300_000
+)  # gas limit used for confirmation transactions
