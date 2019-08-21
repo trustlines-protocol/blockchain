57fd68b6ec54a858de8d35b1f52b710ff23a9e1a
Jannik Luhn <jannik@brainbot.com>
2019-08-21 15:28:16 +0200

    Process control events in recorder

--- tools/bridge/bridge/transfer_recorder.py
+++ tools/bridge/bridge/transfer_recorder.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Set
+import logging
+from typing import Dict, List, Optional, Set
 
 from eth_typing import Hash32
 from web3.datastructures import AttributeDict
@@ -8,11 +9,14 @@ from bridge.constants import (
     CONFIRMATION_EVENT_NAME,
     TRANSFER_EVENT_NAME,
 )
+from bridge.control_events import BalanceCheck, ControlEvent, IsValidatorCheck
 from bridge.utils import compute_transfer_hash
+
+logger = logging.getLogger(__name__)
 
 
 class TransferRecorder:
-    def __init__(self) -> None:
+    def __init__(self, minimum_balance: int) -> None:
         self.transfer_events: Dict[Hash32, AttributeDict] = {}
 
         self.transfer_hashes: Set[Hash32] = set()
@@ -23,13 +27,18 @@ class TransferRecorder:
 
         self.home_chain_synced_until = 0.0
 
-        self.is_validating = False
+        self.minimum_balance = minimum_balance
 
-    def start_validating(self) -> None:
-        if self.is_validating:
-            raise ValueError("Validator is already validating")
+        self.is_validator: Optional[bool] = None
+        self.balance: Optional[int] = None
 
-        self.is_validating = True
+    @property
+    def is_validating(self):
+        return self.is_validator and self.is_balance_sufficient
+
+    @property
+    def is_balance_sufficient(self):
+        return self.balance is not None and self.balance >= self.minimum_balance
 
     def apply_proper_event(self, event: AttributeDict) -> None:
         event_name = event.event
@@ -48,6 +57,25 @@ class TransferRecorder:
             self.completion_hashes.add(transfer_hash)
         else:
             raise ValueError(f"Got unknown event {event}")
+
+    def apply_control_event(self, event: ControlEvent) -> None:
+        if isinstance(event, IsValidatorCheck):
+            if event.is_validator and not self.is_validator:
+                logger.info("Account is a member of the validator set")
+                self.is_validator = True
+            elif not event.is_validator and self.is_validator:
+                logger.info("Account is not a member of the validator set")
+                self.is_validator = False
+
+        elif isinstance(event, BalanceCheck):
+            balance_sufficient_before = self.is_balance_sufficient
+            self.balance = event.balance
+            balance_sufficient_now = self.is_balance_sufficient
+
+            if balance_sufficient_now and not balance_sufficient_before:
+                logger.info("Account balance is sufficiently high")
+            elif not balance_sufficient_now and balance_sufficient_before:
+                logger.info("Account balance is below safe minimum")
 
     def clear_transfers(self) -> None:
         transfer_hashes_to_remove = self.transfer_hashes & self.completion_hashes
