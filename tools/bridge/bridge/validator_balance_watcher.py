c63b075b8d0a900a614b1ae8d85808443b58c58a
Jannik Luhn <jannik@brainbot.com>
2019-08-21 15:28:59 +0200

    Produce control events in balance watcher

--- tools/bridge/bridge/validator_balance_watcher.py
+++ tools/bridge/bridge/validator_balance_watcher.py
@@ -3,17 +3,25 @@ import logging
 import gevent
 from eth_utils import from_wei
 
+from bridge.control_events import BalanceCheck
+
 logger = logging.getLogger(__name__)
 
 
 class ValidatorBalanceWatcher:
     def __init__(
-        self, w3, validator_address, poll_interval, balance_warn_threshold
+        self,
+        w3,
+        validator_address,
+        poll_interval,
+        balance_warn_threshold,
+        control_queue,
     ) -> None:
         self.w3 = w3
         self.validator_address = validator_address
         self.poll_interval = poll_interval
         self.balance_warn_threshold = balance_warn_threshold
+        self.control_queue = control_queue
 
     def run(self) -> None:
         while True:
@@ -23,5 +31,7 @@ class ValidatorBalanceWatcher:
                     f"Low balance of validator account: {from_wei(current_balance, 'ether')} "
                     f"TLC"
                 )
+
+            self.control_queue.put(BalanceCheck(current_balance))
 
             gevent.sleep(self.poll_interval)
