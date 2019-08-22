cd708f61978426f52b226d55d0513658d855f224
Jannik Luhn <jannik@brainbot.com>
2019-08-22 10:57:10 +0200

    Update validator status watcher tests

--- tools/bridge/tests/test_validator_status_watcher.py
+++ tools/bridge/tests/test_validator_status_watcher.py
@@ -2,65 +2,71 @@ from unittest.mock import Mock
 
 import gevent
 from eth_utils import to_canonical_address
+from gevent import Timeout
+from gevent.queue import Queue
 
+from bridge.events import IsValidatorCheck
 from bridge.validator_status_watcher import ValidatorStatusWatcher
 
 
 def test_watcher_checks_initial_validator_status_correctly(
     validator_proxy_with_validators, validator_address, spawn
 ):
-    start_callback = Mock()
+    control_queue = Queue()
     stop_callback = Mock()
     validator_status_watcher = ValidatorStatusWatcher(
         validator_proxy_with_validators,
         to_canonical_address(validator_address),
         poll_interval=1,
-        start_validating_callback=start_callback,
+        control_queue=control_queue,
         stop_validating_callback=stop_callback,
     )
 
     spawn(validator_status_watcher.run)
-    gevent.sleep(0.01)
-    start_callback.assert_called_once()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(True)
     stop_callback.assert_not_called()
 
 
 def test_watcher_checks_initial_non_validator_status_correctly(
     validator_proxy_with_validators, non_validator_address, spawn
 ):
-    start_callback = Mock()
+    control_queue = Queue()
     stop_callback = Mock()
     validator_status_watcher = ValidatorStatusWatcher(
         validator_proxy_with_validators,
         to_canonical_address(non_validator_address),
         poll_interval=1,
-        start_validating_callback=start_callback,
+        control_queue=control_queue,
         stop_validating_callback=stop_callback,
     )
 
     spawn(validator_status_watcher.run)
-    gevent.sleep(0.01)
-    start_callback.assert_not_called()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(False)
     stop_callback.assert_not_called()
 
 
 def test_watcher_notices_validator_set_joining(
     validator_proxy_contract, non_validator_address, system_address, spawn
 ):
-    start_callback = Mock()
+    control_queue = Queue()
     stop_callback = Mock()
     poll_interval = 0.1
     validator_status_watcher = ValidatorStatusWatcher(
         validator_proxy_contract,
         to_canonical_address(non_validator_address),
         poll_interval=poll_interval,
-        start_validating_callback=start_callback,
+        control_queue=control_queue,
         stop_validating_callback=stop_callback,
     )
 
     spawn(validator_status_watcher.run)
-    gevent.sleep(0.01)  # check initial status
-    start_callback.assert_not_called()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(False)
     stop_callback.assert_not_called()
 
     # join validator set
@@ -69,30 +75,31 @@ def test_watcher_notices_validator_set_joining(
     ).transact({"from": system_address})
 
     gevent.sleep(poll_interval * 1.5)  # check a second time
-    start_callback.assert_called_once()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(True)
     stop_callback.assert_not_called()
 
 
 def test_watcher_notices_validator_set_leaving(
     validator_proxy_with_validators, validator_address, system_address, spawn
 ):
-    start_callback = Mock()
+    control_queue = Queue()
     stop_callback = Mock()
     poll_interval = 0.1
     validator_status_watcher = ValidatorStatusWatcher(
         validator_proxy_with_validators,
         to_canonical_address(validator_address),
         poll_interval=poll_interval,
-        start_validating_callback=start_callback,
+        control_queue=control_queue,
         stop_validating_callback=stop_callback,
     )
 
     spawn(validator_status_watcher.run)
-    gevent.sleep(poll_interval * 0.01)  # check initial status
-    start_callback.assert_called_once()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(True)
     stop_callback.assert_not_called()
-
-    start_callback.reset_mock()
 
     # leave validator set
     validator_proxy_with_validators.functions.updateValidators([]).transact(
@@ -100,5 +107,7 @@ def test_watcher_notices_validator_set_leaving(
     )
 
     gevent.sleep(poll_interval)  # check a second time
-    start_callback.assert_not_called()
+    with Timeout(0.1):
+        initial_status_check = control_queue.get()
+    assert initial_status_check == IsValidatorCheck(False)
     stop_callback.assert_called_once()
