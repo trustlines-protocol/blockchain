d407916741e17ed916e74f2fbb3dfd6844de407d
Thore Weilbier <thore@weilbier.net>
2019-08-27 12:25:52 +0200

    Add tests for bridge private key configuration
    These include the syntactic checks of the configuration module as well
    as the semantic checks on decrypting a keystore.
    
    Related to: #354

--- tools/bridge/tests/test_config.py
+++ tools/bridge/tests/test_config.py
@@ -1,4 +1,5 @@
 import pytest
+from eth_keys.constants import SECPK1_N
 from toolz import dissoc
 
 from bridge.config import (
@@ -6,6 +7,7 @@ from bridge.config import (
     validate_config,
     validate_non_negative_integer,
     validate_positive_float,
+    validate_private_key,
     validate_rpc_url,
 )
 
@@ -85,3 +87,83 @@ def test_validate_address_invalid_format():
 def test_validate_address_no_checksum():
     with pytest.raises(ValueError):
         validate_checksum_address("0x4b0b6e093a330c00fe614b804ad59e9b0a4fe8a9")
+
+
+def test_private_key_no_dict():
+    private_key = "0x6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c"
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_raw():
+    private_key = {
+        "raw": "0x6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c"
+    }
+    validate_private_key(private_key)
+
+
+def test_private_key_raw_no_string():
+    private_key = {"raw": 1}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_raw_no_hex():
+    private_key = {"raw": "no_hex"}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_raw_missing_0x_prefix():
+    private_key = {
+        "raw": "6370fd033278c143179d81c5526140625662b8daa446c22ee2d73db3707e620c"
+    }
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_raw_too_short():
+    private_key = {"raw": "0x6370fd033278c143179d"}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_raw_out_of_range():
+    private_key = {"raw": str(hex(SECPK1_N))}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_keystore():
+    private_key = {
+        "keystore_path": "keystore.json",
+        "keystore_password_path": "password",
+    }
+    validate_private_key(private_key)
+
+
+def test_private_key_keystore_no_string():
+    private_key = {"keystore_path": "keystore.json", "keystore_password_path": 1}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_keystore_empty():
+    private_key = {"keystore_path": "keystore.json", "keystore_password_path": ""}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
+
+
+def test_private_key_missing_field():
+    private_key = {"keystore_path": "keystore.json"}
+
+    with pytest.raises(ValueError):
+        validate_private_key(private_key)
