3efccd2b03015ebcc2d648fdafb35f4c9d2f6370
Thore Weilbier <thore@weilbier.net>
2019-09-05 11:31:27 +0200

    Extend quickstart bridge configuration for sync
    Add the optional start block number fields for the generated bridge
    client configuration. Without they are set to zero per default. Because
    the foreign as well as the home node need to sync at the beginning, the
    bridge client remains in a loop of failures. This is caused by the
    attempt to verify the configured contract addresses.
    
    Related to: #302
    See also: #378

--- tools/quickstart/quickstart/bridge.py
+++ tools/quickstart/quickstart/bridge.py
@@ -6,9 +6,11 @@ from quickstart.constants import (
     BRIDGE_CONFIG_FILE_EXTERNAL,
     BRIDGE_CONFIG_FOREIGN_BRIDGE_CONTRACT_ADDRESS,
     BRIDGE_CONFIG_FOREIGN_RPC_URL,
+    BRIDGE_CONFIG_FOREIGN_START_BLOCK_NUMBER,
     BRIDGE_CONFIG_FOREIGN_TOKEN_CONTRACT_ADDRESS,
     BRIDGE_CONFIG_HOME_BRIDGE_CONTRACT_ADDRESS,
     BRIDGE_CONFIG_HOME_RPC_URL,
+    BRIDGE_CONFIG_HOME_START_BLOCK_NUMBER,
     BRIDGE_CONFIG_KEYSTORE_PASSWORD_PATH,
     BRIDGE_CONFIG_KEYSTORE_PATH,
     BRIDGE_DOCUMENTATION_URL,
@@ -20,10 +22,12 @@ ENV_FILE_TEMPLATE = """\
 rpc_url = '{foreign_rpc_url}'
 token_contract_address = '{foreign_token_contract_address}'
 bridge_contract_address = '{foreign_bridge_contract_address}'
+event_fetch_start_block_number = {foreign_start_block_number}
 
 [home_chain]
 rpc_url = '{home_rpc_url}'
 bridge_contract_address = '{home_bridge_contract_address}'
+event_fetch_start_block_number = {home_start_block_number}
 
 [validator_private_key]
 keystore_path = '{keystore_path}'
@@ -56,7 +60,9 @@ def setup_interactively() -> None:
         home_rpc_url=BRIDGE_CONFIG_HOME_RPC_URL,
         foreign_token_contract_address=BRIDGE_CONFIG_FOREIGN_TOKEN_CONTRACT_ADDRESS,
         foreign_bridge_contract_address=BRIDGE_CONFIG_FOREIGN_BRIDGE_CONTRACT_ADDRESS,
+        foreign_start_block_number=BRIDGE_CONFIG_FOREIGN_START_BLOCK_NUMBER,
         home_bridge_contract_address=BRIDGE_CONFIG_HOME_BRIDGE_CONTRACT_ADDRESS,
+        home_start_block_number=BRIDGE_CONFIG_HOME_START_BLOCK_NUMBER,
         keystore_path=BRIDGE_CONFIG_KEYSTORE_PATH,
         keystore_password_path=BRIDGE_CONFIG_KEYSTORE_PASSWORD_PATH,
     )
