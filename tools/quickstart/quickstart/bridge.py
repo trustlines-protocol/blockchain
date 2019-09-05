3401916c2f0be143ce62116c483dc05fe323b5bc
Thore Weilbier <thore@weilbier.net>
2019-09-05 16:48:18 +0200

    Use dump a dictionary as quickstart bridge config
    Related to: #378

--- tools/quickstart/quickstart/bridge.py
+++ tools/quickstart/quickstart/bridge.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from typing import Dict
 
 import click
+import toml
 
 from quickstart.constants import (
     BRIDGE_CONFIG_FILE_EXTERNAL,
@@ -16,23 +18,6 @@ from quickstart.constants import (
     BRIDGE_DOCUMENTATION_URL,
 )
 from quickstart.utils import is_bridge_prepared
-
-ENV_FILE_TEMPLATE = """\
-[foreign_chain]
-rpc_url = '{foreign_rpc_url}'
-token_contract_address = '{foreign_token_contract_address}'
-bridge_contract_address = '{foreign_bridge_contract_address}'
-event_fetch_start_block_number = {foreign_start_block_number}
-
-[home_chain]
-rpc_url = '{home_rpc_url}'
-bridge_contract_address = '{home_bridge_contract_address}'
-event_fetch_start_block_number = {home_start_block_number}
-
-[validator_private_key]
-keystore_path = '{keystore_path}'
-keystore_password_path = '{keystore_password_path}'
-"""
 
 
 def setup_interactively() -> None:
@@ -55,18 +40,49 @@ def setup_interactively() -> None:
         Path(BRIDGE_CONFIG_FILE_EXTERNAL).touch()
         return
 
-    env_file_content = ENV_FILE_TEMPLATE.format(
-        foreign_rpc_url=BRIDGE_CONFIG_FOREIGN_RPC_URL,
-        home_rpc_url=BRIDGE_CONFIG_HOME_RPC_URL,
-        foreign_token_contract_address=BRIDGE_CONFIG_FOREIGN_TOKEN_CONTRACT_ADDRESS,
-        foreign_bridge_contract_address=BRIDGE_CONFIG_FOREIGN_BRIDGE_CONTRACT_ADDRESS,
-        foreign_start_block_number=BRIDGE_CONFIG_FOREIGN_START_BLOCK_NUMBER,
-        home_bridge_contract_address=BRIDGE_CONFIG_HOME_BRIDGE_CONTRACT_ADDRESS,
-        home_start_block_number=BRIDGE_CONFIG_HOME_START_BLOCK_NUMBER,
-        keystore_path=BRIDGE_CONFIG_KEYSTORE_PATH,
-        keystore_password_path=BRIDGE_CONFIG_KEYSTORE_PASSWORD_PATH,
+    configuration = get_bridge_configuration(
+        BRIDGE_CONFIG_FOREIGN_RPC_URL,
+        BRIDGE_CONFIG_FOREIGN_TOKEN_CONTRACT_ADDRESS,
+        BRIDGE_CONFIG_FOREIGN_BRIDGE_CONTRACT_ADDRESS,
+        BRIDGE_CONFIG_FOREIGN_START_BLOCK_NUMBER,
+        BRIDGE_CONFIG_HOME_RPC_URL,
+        BRIDGE_CONFIG_HOME_BRIDGE_CONTRACT_ADDRESS,
+        BRIDGE_CONFIG_HOME_START_BLOCK_NUMBER,
+        BRIDGE_CONFIG_KEYSTORE_PATH,
+        BRIDGE_CONFIG_KEYSTORE_PASSWORD_PATH,
     )
 
-    with open(BRIDGE_CONFIG_FILE_EXTERNAL, "w") as env_file:
-        env_file.write(env_file_content)
+    with open(BRIDGE_CONFIG_FILE_EXTERNAL, "w") as config_file:
+        toml.dump(configuration, config_file)
+
     click.echo("Bridge client setup complete.")
+
+
+def get_bridge_configuration(
+    foreign_rpc_url: str,
+    foreign_token_contract_address: str,
+    foreign_bridge_contract_address: str,
+    foreign_start_block_number: int,
+    home_rpc_url: str,
+    home_bridge_contract_address: str,
+    home_start_block_number: int,
+    keystore_path: str,
+    keystore_password_path: str,
+) -> Dict:
+    return {
+        "foreign_chain": {
+            "rpc_url": foreign_rpc_url,
+            "token_contract_address": foreign_token_contract_address,
+            "bridge_contract_address": foreign_bridge_contract_address,
+            "event_fetch_start_block_number": foreign_start_block_number,
+        },
+        "home_chain": {
+            "rpc_url": home_rpc_url,
+            "bridge_contract_address": home_bridge_contract_address,
+            "event_fetch_start_block_number": home_start_block_number,
+        },
+        "validator_private_key": {
+            "keystore_path": keystore_path,
+            "keystore_password_path": keystore_password_path,
+        },
+    }
