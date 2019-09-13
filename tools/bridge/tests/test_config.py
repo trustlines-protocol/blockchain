52cc29aad2074b3a4cd7a835bd41c492630c903a
Ralf Schmitt <ralf@brainbot.com>
2019-09-13 21:46:46 +0200

    Test loading of 'logging' config

--- tools/bridge/tests/test_config.py
+++ tools/bridge/tests/test_config.py
@@ -14,6 +14,21 @@ bridge_contract_address = "0x771434486a221c6146F27B72fd160Bdf0eb1288e"
 raw = "0xb8dcbb8a564483279579e04bffacbd76f79df157cfbebed84079673b32d9e72f"
 """
 
+example_logging_config = """
+[logging.root]
+level = "DEBUG"
+
+[logging.loggers."bridge.main"]
+level = "DEBUG"
+
+# web3 is too verbose with level debug
+[logging.loggers.web3]
+level = "INFO"
+
+[logging.loggers.urllib3]
+level = "INFO"
+"""
+
 
 def test_load_minimal_config(write_config):
     cfg = bridge.config.load_config(write_config(minimal_config))
@@ -29,3 +44,13 @@ def test_load_minimal_config(write_config):
 
     assert cfg["foreign_chain"]["event_fetch_start_block_number"] == 0
     assert cfg["foreign_chain"]["max_reorg_depth"] >= 10
+
+
+def test_logging_key(write_config):
+    cfg = bridge.config.load_config(
+        write_config(minimal_config + "\n" + example_logging_config)
+    )
+    print(cfg)
+    assert cfg["logging"]["version"] == 1
+    assert cfg["logging"]["incremental"] is True
+    assert cfg["logging"]["loggers"]["bridge.main"] == {"level": "DEBUG"}
