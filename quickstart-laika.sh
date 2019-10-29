fc105138b6e69886c9b4d23184ca9e908a5ff4b7
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-10-29 17:52:00 +0100

    Update quickstart scripts

--- quickstart-laika.sh
+++ quickstart-laika.sh
@@ -9,7 +9,7 @@
 set -e
 
 # Variables
-: "${DOCKER_IMAGE:=trustlines/quickstart:master16567}"
+: "${DOCKER_IMAGE:=trustlines/quickstart:master16896}"
 : "${DATA_DIR:=${PWD}/trustlines}"
 GREEN='\033[0;32m'
 RESET='\033[0m'
