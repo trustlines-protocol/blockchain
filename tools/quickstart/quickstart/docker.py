878aa02374f693af03c9da0695c1c4b25c2e249b
Jannik Luhn <jannik@brainbot.com>
2019-11-05 11:33:32 +0700

    Add and use author address option in docker file

--- tools/quickstart/quickstart/docker.py
+++ tools/quickstart/quickstart/docker.py
@@ -15,7 +15,7 @@ from quickstart.utils import (
     is_validator_account_prepared,
     show_file_diff,
 )
-from quickstart.validator_account import get_validator_address
+from quickstart.validator_account import get_author_address, get_validator_address
 
 # List of docker container names to stop and remove on startup in addition to the ones defined in
 # the docker compose file (for backward compatibility)
@@ -98,6 +98,7 @@ def update_and_start(
         env_variables = {
             **default_env_vars,
             "VALIDATOR_ADDRESS": get_validator_address(base_dir),
+            "AUTHOR_ADDRESS": get_author_address(base_dir),
             "ROLE": "validator",
         }
         click.echo("\nNode will run as a validator")
