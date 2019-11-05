21ab0bfc3819cb690ee2d5a2baaa619c6f0240a0
Ralf Schmitt <ralf@brainbot.com>
2019-11-05 07:13:35 +0100

    Merge PR #540: check address in parity_wrapper.sh

--- chain/parity_wrapper.sh
+++ chain/parity_wrapper.sh
@@ -104,6 +104,17 @@ function checkRoleArgument() {
   exit 1
 }
 
+# Check if the set address is a valid address
+# Does not do a checksum test
+#
+function checkAddressArgument() {
+  [[ $ADDRESS =~ ^0x[0-9a-fA-F]{40}$ ]] && return
+
+  # Error report to the user with the correct usage.
+  echo "The defined address ('$ADDRESS') is invalid."
+  exit 1
+}
+
 # Parse the arguments, given to the script by the caller.
 # Not defined configuration values stay with their default values.
 # A not known argument leads to an exit with status code 1.
@@ -131,8 +142,9 @@ function parseArguments() {
     elif [[ $arg == --address ]] || [[ $arg == -a ]]; then
       # Take the next argument as the address and jump other it.
       ADDRESS="$2"
-      shift # arg
-      shift # value
+      checkAddressArgument # Make sure to have a valid address.
+      shift                # arg
+      shift                # value
 
     # Additional arguments for the Parity client.
     # Use all remain arguments for parity.
