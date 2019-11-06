c214adab27fffb94608e27db3b04dedd72240314
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-11-06 15:07:35 +0100

    Fix not always starting a light node on new config
    Ask a user again after overwriting the bridge config if he wants to run
    a foreign light node. Before this was broken, because the user would
    then suddently have a light node running

--- tools/quickstart/quickstart/bridge.py
+++ tools/quickstart/quickstart/bridge.py
@@ -27,73 +27,58 @@ def setup_interactively(base_dir, bridge_config_file, foreign_chain_name) -> Non
         click.echo("\nThe bridge client has already been set up.")
         if file_hash(user_file) in LEGACY_CONFIG_HASHES:
             # Override former config files without asking
-            copy_default_bridge_config(base_dir, bridge_config_file)
+            copy_default_bridge_config(user_file, bridge_config_file)
+            return
         elif filecmp.cmp(user_file, bridge_config_file):
             # same file, so nothing to do
-            pass
+            return
         else:
             # Custom changes
-            _show_file_override_dialog(base_dir, bridge_config_file)
-        return
+            if not _should_overwrite_prompt(base_dir, bridge_config_file):
+                # not overwriting, so nothing to do
+                return
+    else:
+        if not is_validator_account_prepared(base_dir):
+            click.echo(
+                "\nNo bridge node will be set up when running as a non-validator."
+            )
+            return
 
-    if not is_validator_account_prepared(base_dir):
-        click.echo("\nNo bridge node will be set up when running as a non-validator.")
-        return
-
-    click.echo(
-        "\n".join(
-            (
-                "",
-                fill(
-                    "As a validator of the Trustlines blockchain, you are required to run a "
-                    "bridge node. Checkout the following link for more information:"
-                ),
-                BRIDGE_DOCUMENTATION_URL,
-                "",
+        click.echo(
+            "\n".join(
+                (
+                    "",
+                    fill(
+                        "As a validator of the Trustlines blockchain, you are required to run a "
+                        "bridge node. Checkout the following link for more information:"
+                    ),
+                    BRIDGE_DOCUMENTATION_URL,
+                    "",
+                )
             )
         )
-    )
-    if not click.confirm(
-        "Do you want to set up a bridge node now? (recommended)", default=True
-    ):
-        # Necessary to make docker-compose not complain about it.
-        Path(user_file).touch()
-        return
+        if not click.confirm(
+            "Do you want to set up a bridge node now? (recommended)", default=True
+        ):
+            # Necessary to make docker-compose not complain about it.
+            Path(user_file).touch()
+            return
 
-    choice = click.prompt(
-        fill(
-            f"The bridge needs a a connection to a(n) {foreign_chain_name} node.\n"
-            f"Do you want to run a light node (1) (default) "
-            f"or do you already have a node running and want to connect via JSON rpc (2) ?"
-        )
-        + "\n",
-        type=click.Choice(("1", "2")),
-        show_choices=False,
-        default="1",
-        show_default=False,
+    _write_bridge_config_dialog(
+        base_dir=base_dir,
+        bridge_config_file=bridge_config_file,
+        user_file=user_file,
+        foreign_chain_name=foreign_chain_name,
     )
-    if choice == "1":
-        copy_default_bridge_config(base_dir, bridge_config_file)
-        assert is_bridge_using_template_json_rpc_url(base_dir, bridge_config_file)
-    elif choice == "2":
-        url = read_json_rpc_url(bridge_config_file)
-        # We want to ask for the URL before we copy the default config in case the user exits while entering the url
-        # and wishes to restart the process
-        copy_default_bridge_config(base_dir, bridge_config_file)
-        change_json_rpc_url(user_file, url)
-    else:
-        assert False, "unreachable"
 
     click.echo("Bridge client setup complete.")
 
 
-def copy_default_bridge_config(base_dir, bridge_config_file):
-    shutil.copyfile(
-        bridge_config_file, os.path.join(base_dir, BRIDGE_CONFIG_FILE_EXTERNAL)
-    )
+def copy_default_bridge_config(user_file, bridge_config_file):
+    shutil.copyfile(bridge_config_file, user_file)
 
 
-def _show_file_override_dialog(base_dir, bridge_config_file):
+def _should_overwrite_prompt(base_dir, bridge_config_file):
     while True:
         choice = click.prompt(
             fill(
@@ -107,11 +92,9 @@ def _show_file_override_dialog(base_dir, bridge_config_file):
         )
 
         if choice == "1":
-            copy_default_bridge_config(base_dir, bridge_config_file)
-            break
+            return True
         elif choice == "2":
-            # Nothing to do
-            break
+            return False
         elif choice == "3":
             show_file_diff(
                 os.path.join(base_dir, BRIDGE_CONFIG_FILE_EXTERNAL),
@@ -120,6 +103,34 @@ def _show_file_override_dialog(base_dir, bridge_config_file):
             )
         else:
             assert False, "unreachable"
+
+
+def _write_bridge_config_dialog(
+    *, base_dir, bridge_config_file, user_file, foreign_chain_name
+):
+    choice = click.prompt(
+        fill(
+            f"The bridge needs a a connection to a(n) {foreign_chain_name} node.\n"
+            f"Do you want to run a light node (1) (default) "
+            f"or do you already have a node running and want to connect via JSON rpc (2) ?"
+        )
+        + "\n",
+        type=click.Choice(("1", "2")),
+        show_choices=False,
+        default="1",
+        show_default=False,
+    )
+    if choice == "1":
+        copy_default_bridge_config(user_file, bridge_config_file)
+        assert is_bridge_using_template_json_rpc_url(base_dir, bridge_config_file)
+    elif choice == "2":
+        url = read_json_rpc_url(bridge_config_file)
+        # We want to ask for the URL before we copy the default config in case the user exits while entering the url
+        # and wishes to restart the process
+        copy_default_bridge_config(user_file, bridge_config_file)
+        change_json_rpc_url(user_file, url)
+    else:
+        assert False, "unreachable"
 
 
 def read_json_rpc_url(bridge_config_file):
