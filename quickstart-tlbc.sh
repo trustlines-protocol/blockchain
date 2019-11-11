43887eba4712ea1b4036d33085670c1148281194
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-11-11 15:13:50 +0100

    Update quickstart script version

--- quickstart-tlbc.sh
+++ quickstart-tlbc.sh
@@ -9,7 +9,7 @@
 set -e
 
 # Variables
-: "${DOCKER_IMAGE:=trustlines/quickstart:master19077}"
+: "${DOCKER_IMAGE:=trustlines/quickstart:master19609}"
 : "${DATA_DIR:=${PWD}/tlbc}"
 GREEN='\033[0;32m'
 RESET='\033[0m'
