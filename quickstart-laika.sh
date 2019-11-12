228ebd1db0edc1ccced0f84da3e102385b38ceb1
Bernd Bohmeier <berndbohmeier@googlemail.com>
2019-11-12 13:08:15 +0100

    Update quickstart to new version

--- quickstart-laika.sh
+++ quickstart-laika.sh
@@ -9,7 +9,7 @@
 set -e
 
 # Variables
-: "${DOCKER_IMAGE:=trustlines/quickstart:master19721}"
+: "${DOCKER_IMAGE:=trustlines/quickstart:master19949}"
 : "${DATA_DIR:=${PWD}/trustlines}"
 GREEN='\033[0;32m'
 RESET='\033[0m'
