b0198377ebccb192e7ad2568dd62b4e78ef7e831
Bernd Bohmeier <berndbohmeier@googlemail.com>
2020-05-14 14:19:39 +0200

    Increase transaction gas limit for bridge txs
    Increase transaction gas limit to account for increased gas costs in
    Istanbul fork.
    Loosen bridge dependencies restriction, as they were stricter than
    necessary.

--- tools/bridge/src/bridge/constants.py
+++ tools/bridge/src/bridge/constants.py
@@ -6,9 +6,9 @@ COMPLETION_EVENT_NAME = "TransferCompleted"
 
 # Gas limit used for confirmation transactions. The actual gas usage can be determined with
 # test_measure_gas_home_bridge.py found in the smart contract test directory. Currently this is
-# 422074, but we add a generous safety margin.
+# 422074, but will increase to 630437 after Istanbul fork on home chain
 # On changing this value, update the corresponding constant in the test script accordingly.
-CONFIRMATION_TRANSACTION_GAS_LIMIT = 500_000
+CONFIRMATION_TRANSACTION_GAS_LIMIT = 650_000
 
 # maximum amount of time in seconds application greenlets have to cleanup before shutdown
 APPLICATION_CLEANUP_TIMEOUT = 5
